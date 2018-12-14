e01acc15b5e02616f87aad9cd36393f63ead7a23
Lyago Monteiro <lyagosilva@gmail.com>
2018-12-14 05:05:23 -0200

    Version 0.21 - Animation and coding polishing
    Cut-off some startup frames from striking animations. Renamed adventurer class with Adventure.

--- HitNBeat/MyWorld.java
+++ HitNBeat/MyWorld.java
@@ -2,8 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
@@ -16,10 +16,10 @@ public class MyWorld extends World
     // Controlls the beat
     private int tempoUnitsCount = 0;
     private GreenfootSound backgroundMusic;
-    
-    
+
+
     // Controlling actors
-    private adventurer p1;
+    private Adventurer p1;
     private Life       life;
     private Score      score;
     private MenuButton button;
@@ -27,22 +27,22 @@ public class MyWorld extends World
     private Completed   completed;
     private FinalScore finalScore;
     // Game state
-    private enum GameState{MENU,PLAYING,GAMEOVER, COMPLETED};    
+    private enum GameState{MENU,PLAYING,GAMEOVER, COMPLETED};
     private GameState state;
-    
+
     /**
      * Constructor for objects of class MyWorld.
-     * 
+     *
      */
     public MyWorld()
-    {    
+    {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Greenfoot.setSpeed(50);
         // Set the initial world state to MENU
-        prepareMenu();  
+        prepareMenu();
     }
-    
+
     private void prepareMenu(){
         // Clear the world
         removeObjects(getObjects(Actor.class));
@@ -51,42 +51,42 @@ public class MyWorld extends World
         GreenfootImage bgImage = new GreenfootImage("menu_background.png");
         bgImage.scale(bgImage.getWidth()+100, bgImage.getHeight()+100 );
         background.setImage(bgImage);
-        addObject(background,1*getWidth()/8,getHeight()/2); 
+        addObject(background,1*getWidth()/8,getHeight()/2);
         MenuAdventurer adventurer = new MenuAdventurer();
-        GreenfootImage adventurerImage = new GreenfootImage("adventurer-die-04.png");
+        GreenfootImage adventurerImage = new GreenfootImage("Adventurer-die-04.png");
         adventurerImage.scale(adventurerImage.getWidth()+30, adventurerImage.getHeight()+30 );
         adventurer.setImage(adventurerImage);
         addObject(adventurer,2*getWidth()/8+10,getHeight()/2-20);
         // Create the menu button
-        button = new MenuButton();        
+        button = new MenuButton();
         addObject(button,getWidth()-60,getHeight()-60);
-        
-        
-        
+
+
+
         // Set the game state
         state = GameState.MENU;
     }
     private void preparePlaying(){
         // Clear the world
         removeObjects(getObjects(Actor.class));
-        
+
         // background
         PlayingBackground background = new PlayingBackground();
         GreenfootImage bgImage = new GreenfootImage("playing_background.png");
         bgImage.scale(bgImage.getWidth() + 800, bgImage.getHeight() +800);
         background.setImage(bgImage);
         addObject(background,3*getWidth()/4,2*getHeight()/3);
-        
+
         Metronome metronome = new Metronome();
         addObject(metronome,getWidth()/3,4*getHeight()/7);
-        p1 = new adventurer("right","up","left", 6);
+        p1 = new Adventurer("right","up","left", 4);
         addObject(p1,getWidth()/6,getHeight()/2);
         score= new Score();
         life = new Life();
         addObject(score,getWidth()/2,15);
         addObject(life,getWidth()/10,15);
-        
-        
+
+
         // Set the game state
         state = GameState.PLAYING;
         Greenfoot.start();
@@ -98,41 +98,41 @@ public class MyWorld extends World
     }
     private void prepareGameOver(){
         // Clear the world
-        removeObjects(getObjects(Actor.class)); 
-        
+        removeObjects(getObjects(Actor.class));
+
         //Stop music
         backgroundMusic.stop();
-      
+
         // Add gameover screen
         gameOver = new GameOver();
         addObject(gameOver,getWidth()/2,getHeight()/2);
-        
+
         // Add the final score
         int fscore = score.getScore();
         finalScore = new FinalScore(fscore);
         addObject(finalScore,getWidth()/2,getHeight()/2);
-        
+
         // Set the game state
-        state = GameState.GAMEOVER;           
+        state = GameState.GAMEOVER;
     }
     private void prepareCompleted(){
         // Clear the world
-        removeObjects(getObjects(Actor.class)); 
-        
+        removeObjects(getObjects(Actor.class));
+
         //Stop music
         backgroundMusic.stop();
-      
+
         // Add gameover screen
         completed = new Completed();
         addObject(completed,getWidth()/2,getHeight()/2);
-        
+
         // Add the final score
         int fscore = score.getScore();
         finalScore = new FinalScore(fscore);
         addObject(finalScore,getWidth()/2,getHeight()/2);
-        
+
         // Set the game state
-        state = GameState.GAMEOVER;           
+        state = GameState.GAMEOVER;
     }
     public void setTempo(int tempo){
         this.tempo = tempo;
@@ -140,15 +140,15 @@ public class MyWorld extends World
     // ========================================================================== //
     // Game events                                                                //
     // These methods are invoked to proccess main game events.                    //
-    // ========================================================================== //   
+    // ========================================================================== //
     //Dictates when a input is gonna be considered on or off beat
     public boolean isActionTime()
-    {   
+    {
         Metronome metronome = this.getObjects(Metronome.class).get(0);
         TempoUnit unit = metronome.getNearTempoUnit();
         if(unit != null){
             if(unit.isActionTime()){
-                frame++;    
+                frame++;
                 return true;
             }
         //Right now there is a 15 frame window for the input be considered on beat.
@@ -157,24 +157,24 @@ public class MyWorld extends World
             frame = 0;
             return true;
         }
-        
+
         return false;
     }
     public void playerHit()
     {
         score.addScore(5);
     }
-    
+
     public void playerGotHit()
     {
         life.takeLife();
-        
+
         if (life.isDead())
         {
             prepareGameOver();
         }
     }
-    
+
     public void act()
     {
         switch(state)
@@ -183,13 +183,13 @@ public class MyWorld extends World
             {
                 actMenu();
                 break;
-            }            
+            }
             case PLAYING:
-            {                  
-                
-                actPlaying();   
+            {
+
+                actPlaying();
                 break;
-            }            
+            }
             case GAMEOVER:
             {
                 actGameOver();
@@ -209,24 +209,24 @@ public class MyWorld extends World
             preparePlaying();
         }
     }
-    
+
     public void actPlaying()
     {
         createTempoUnit(tempo, Greenfoot.getRandomNumber(3));
-        
+
         if(Greenfoot.isKeyDown("escape")){
             Greenfoot.stop();
             backgroundMusic.stop();
         }
-        
+
         if(!backgroundMusic.isPlaying()){
             prepareCompleted();
         }
-      
+
     }
     public void createTempoUnit(int tempo, int classe){
         if (tempoUnitsCount == 0)
-        {   
+        {
             TempoUnit unit;
             if (classe == 0){
                 unit = new Arrow(this.getWidth());
@@ -244,20 +244,20 @@ public class MyWorld extends World
                 axe.scale(axe.getWidth() - 100, axe.getHeight() - 100);
                 unit.setImage(axe);
             }
-            
-            
+
+
             this.addObject(unit,this.getWidth(),4*getHeight()/7);
 
-                     
+
         }
-        
+
         // Synchronizes spawn time off TempoUnits with tempo (60frames x 60sec/bpm)
         if (tempoUnitsCount++ == 3600/tempo)
         {
             tempoUnitsCount=0   ;
-        }   
+        }
     }
-    
+
     public void actGameOver()
     {
         if(Greenfoot.mouseClicked(gameOver) ||
@@ -280,7 +280,7 @@ public class MyWorld extends World
            //TODO: file is playing multiple times when a offbeat input happens
 
                Greenfoot.playSound("sfx_missedbeat_01.mp3");
-               
+
 
            return true;
        }
